9a75a4c1853371bcd57582c2b9a3a871ac0365dc
mangonceb <manuel.gonceb@educa.jcyl.es>
2021-09-28 11:33:27 +0200

    first commit

---
+++ js/script1.js
@@ -0,0 +1,17 @@
+alert("HOLA");
+
+//Elementos cambiar tipografía
+let elemento = document.getElementById("nombre");
+let nombreOr = elemento.innerText;
+
+//Funciones cambiar tipografía
+elemento.addEventListener("mouseover", function () {
+    elemento.style.color = "green";
+    elemento.innerText = (nombreOr).toUpperCase();
+}, false);
+
+elemento.addEventListener("mouseout", function () {
+    elemento.style.color = "black";
+    elemento.innerText = nombreOr;
+}, false)
+
